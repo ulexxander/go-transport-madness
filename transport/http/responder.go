3ba993dd592888a5080a1471115d0ad39ca97df4
Alexander Ustyugov <ulexxander@gmail.com>
2021-07-28 21:47:16 +0200

    transport/http,graphql - minor refactoring

--- transport/http/responder.go
+++ transport/http/responder.go
@@ -1,12 +1,9 @@
 package http
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
-	"github.com/pkg/errors"
 	"github.com/ulexxander/transport-madness/services"
 )
 
@@ -72,56 +69,4 @@ func (rs *Responder) Setup() {
 		}
 		rs.respondData(w, data)
 	})
-}
-
-func requestBody(r *http.Request, out interface{}) error {
-	d := json.NewDecoder(r.Body)
-	d.DisallowUnknownFields()
-	if err := d.Decode(out); err != nil {
-		return errors.Wrap(err, "invalid body")
-	}
-	return nil
-}
-
-func queryInt(r *http.Request, key string) (int, error) {
-	valStr := r.URL.Query().Get(key)
-	if valStr == "" {
-		return 0, errors.Errorf("query parameter %s is missing", key)
-	}
-	valInt, err := strconv.Atoi(valStr)
-	if err != nil {
-		return 0, err
-	}
-	return valInt, nil
-}
-
-type responseSuccess struct {
-	Data interface{}
-}
-
-type responseError struct {
-	// TODO: error codes and http response codes
-	Error string
-}
-
-func (r *Responder) respondData(w http.ResponseWriter, data interface{}) {
-	res := responseSuccess{
-		Data: data,
-	}
-	r.respond(w, res)
-}
-
-func (r *Responder) respondError(w http.ResponseWriter, err error) {
-	res := responseError{
-		Error: err.Error(),
-	}
-	r.respond(w, res)
-}
-
-func (r *Responder) respond(w http.ResponseWriter, payload interface{}) {
-	if err := json.NewEncoder(w).Encode(payload); err != nil {
-		r.Log.Println("error when encoding http response:", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
 }
