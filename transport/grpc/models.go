e259b3098ebbc72e219adb76ca7f4447bcaedbb3
Alexander Ustyugov <ulexxander@gmail.com>
2021-07-29 23:11:16 +0200

    transport/grpc: implemented MessagesServer

--- transport/grpc/models.go
+++ transport/grpc/models.go
@@ -12,10 +12,26 @@ func ConvertUser(mu models.User) *pb.User {
 	}
 }
 
-func ConvertUsers(mu []models.User) *pb.UserAllReply {
+func ConvertUsers(mu []models.User) []*pb.User {
 	var pu []*pb.User
 	for _, u := range mu {
 		pu = append(pu, ConvertUser(u))
 	}
-	return &pb.UserAllReply{Users: pu}
+	return pu
+}
+
+func ConvertMessage(mm models.Message) *pb.Message {
+	return &pb.Message{
+		SenderUsername: mm.SenderUsername,
+		Content:        mm.Content,
+		CreatedAt:      mm.CreatedAt.String(),
+	}
+}
+
+func ConvertMessages(mm []models.Message) []*pb.Message {
+	var pm []*pb.Message
+	for _, m := range mm {
+		pm = append(pm, ConvertMessage(m))
+	}
+	return pm
 }
--- transport/grpc/responder.go
+++ transport/grpc/responder.go
@@ -28,7 +28,7 @@ type UsersServer struct {
 
 func (us *UsersServer) All(ctx context.Context, _ *pb.Void) (*pb.UserAllReply, error) {
 	data := us.UsersService.UsersAll()
-	return ConvertUsers(data), nil
+	return &pb.UserAllReply{Users: ConvertUsers(data)}, nil
 }
 
 func (us *UsersServer) Create(ctx context.Context, req *pb.UserCreateRequest) (*pb.UserCreateReply, error) {
@@ -44,4 +44,30 @@ func (us *UsersServer) Create(ctx context.Context, req *pb.UserCreateRequest) (*
 type MessagesServer struct {
 	pb.UnimplementedMessagesServer
 	MessagesService *services.MessagesService
+}
+
+// impl: ms *MessagesServer pb.MessagesServer
+
+func (ms *MessagesServer) Pagination(ctx context.Context, req *pb.MessagesPaginationRequest) (*pb.MessagePaginationReply, error) {
+	data, err := ms.MessagesService.MessagesPagination(services.MessagesPaginationInput{
+		Page:     int(req.Page),
+		PageSize: int(req.PageSize),
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.MessagePaginationReply{
+		Messages: ConvertMessages(data),
+	}, nil
+}
+
+func (ms *MessagesServer) Create(ctx context.Context, req *pb.MessageCreateRequest) (*pb.MessageCreateReply, error) {
+	data, err := ms.MessagesService.CreateMessage(services.MessageCreateInput{
+		SenderUsername: req.SenderUsername,
+		Content:        req.Content,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.MessageCreateReply{Message: ConvertMessage(*data)}, nil
 }
